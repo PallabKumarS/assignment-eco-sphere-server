480feefe6a67433229f9f8d980728997094e59b7
PallabKumarS <pallabkumar26@gmail.com>
2025-05-09 21:55:47 +0600

    fix: resolve shurjopay typescript issues
    - Adds eslint disable comments to suppress no-unused-vars errors in `src/app/interface/shurjoPay.d.ts`.

--- src/app/interface/shurjoPay.d.ts
+++ src/app/interface/shurjoPay.d.ts
@@ -1,5 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable no-unused-vars */
 declare module 'shurjopay' {
   import { Logger } from 'winston';
 
